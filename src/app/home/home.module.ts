696cde1b760c47812212efc49940bdd735da68f6
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-10-21 17:30:35 -0400

    Add partners component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from './banner/banner.component';
+import { PartnersComponent } from './partners/partners.component';
+import { PartnerService } from './partners/partner.service';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule]
+  declarations: [HomeComponent, BannerComponent, PartnersComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  providers: [PartnerService]
 })
 export class HomeModule {}
